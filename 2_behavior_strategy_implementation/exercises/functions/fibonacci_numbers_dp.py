900ae62a787bcc385834e5d1848c408392b9049a
WuorBhang <uhuribhang211@gmail.com>
2025-03-11 14:01:58 +0300

    updates

--- 2_behavior_strategy_implementation/exercises/functions/fibonacci_numbers_dp.py
+++ 2_behavior_strategy_implementation/exercises/functions/fibonacci_numbers_dp.py
@@ -5,7 +5,7 @@
 
 # --- declare function ---
 
-def fibonacci_numbers_dp(n):
+def fibonacci_numbers_dp(n: int) -> int: 
     f = [0, 1]
     for i in range(2, n+1):
         f.append(f[i-1] + f[i-2])
